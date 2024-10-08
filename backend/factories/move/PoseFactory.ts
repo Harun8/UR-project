a35f836accb51da013379f9691b458a000a39279
Harun <harunabdi8@gmail.com>
2024-10-08 20:03:54 +0200

    regular commit

--- backend/factories/move/PoseFactory.ts
+++ backend/factories/move/PoseFactory.ts
@@ -2,29 +2,6 @@ import { Pose, PoseValue } from "../../interfaces/waypoint";
 import { isArray } from "../../utils/ArrayChecker";
 import { formatMatrix, forwardKinematics } from "../../utils/Kinematics";
 export class PoseFactory {
-  static createPose(poseStr: string): Pose {
-    const values = poseStr.split(",").map((val) => parseFloat(val.trim()));
-    const units = ["m", "m", "m", "rad", "rad", "rad"];
-
-    const poseValue = (index: number): PoseValue => ({
-      entity: {
-        value: values[index],
-        unit: units[index],
-      },
-      selectedType: "VALUE",
-      value: values[index],
-    });
-
-    return {
-      x: poseValue(0),
-      y: poseValue(1),
-      z: poseValue(2),
-      rx: poseValue(3),
-      ry: poseValue(4),
-      rz: poseValue(5),
-    };
-  }
-
   static getKinematicPose(kinematicsData: any) {
     try {
       if (!kinematicsData) {
