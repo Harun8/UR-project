58cffb1d165cd950efb2d8c7f5ab49b3e1d074bf
Harun <harunabdi8@gmail.com>
2024-11-18 15:09:20 +0100

    frontend and some backend

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,29 +1,38 @@
-import Attachment from "../components/Attachment";
-import Nav from "../components/Nav";
+import ShineBorder from "../components/ui/shine-border";
+import "../styles/Homepage.css"
+import FileUpload from "../components/FileUpload"
+import { useState } from "react";
 
 const HomePage = () => {
+  const [convertFile, setConvertFile] = useState(false)
   return (
-    <body className="min-h-screen w-full bg-sky-200">
-      <Nav></Nav>
-      <>
-        <div className="grid h-lvh grid-cols-2 gap-12 content-center">
-          {/* Text here */}
-          <div className="flex flex-col items-center ">
-            <h2 className="text-2xl font-bold text-sky-800">
-              PS5 to PSX converter
-            </h2>
-            <div className="flex justfiy-center">
-              <p className="text-md font-semibold text-gray-700 hover:text-black tracking-wide	mx-24 mt-12">
-                Upload your existing PS5 program, and let us convert it to PSX
-                for you!
-              </p>
-            </div>
-          </div>
+    <>
+      <div className=" bg-white flex flex-col py-24 items-center text-center min-h-screen">
+        <h1 className="text-7xl font-bold font-sans mb-4 font-extrabold	 text-gray-800 ">
+         <span className="title"> PolyScope X </span><br></br>
+          <span> Converter</span>
+        </h1>
+        <p className="text-gray-500 mb-8"> Convert your PolyScope 5 files to PolyScope x</p>
 
-          <Attachment></Attachment>
+        <div className="flex space-x-4">
+          <button onClick={() => setConvertFile(prev => !prev)}
+          
+            className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-3 px-10 rounded-lg hover:bg-gray-800">
+            Click here to convert
+          </button>
+          <button
+           
+            className="border border-gray-500 text-gray-950 font-bold py-3 px-10  rounded-lg hover:bg-gray-100">
+            Read more
+          </button>
         </div>
+        <div className=" mt-12 w-[1000px] h-[100px] flex justify-center">
+     
+        <FileUpload convertFile={convertFile}></FileUpload>
+        </div>
+      </div>
+
       </>
-    </body>
   );
 };
 
